0d2875e32ae98c820963c831496725bb75a077f8
Zsomi <zsomidev@gmail.com>
2023-11-25 12:32:27 +0100

    Start

---
+++ src/main/java/hu/zsomi/gensfarm/gensfarm/GensFarm.java
@@ -0,0 +1,40 @@
+package hu.zsomi.gensfarm.gensfarm;
+
+import com.artillexstudios.axapi.AxPlugin;
+import com.artillexstudios.axapi.config.Config;
+import com.artillexstudios.axapi.libs.boostedyaml.boostedyaml.dvs.versioning.BasicVersioning;
+import com.artillexstudios.axapi.libs.boostedyaml.boostedyaml.settings.dumper.DumperSettings;
+import com.artillexstudios.axapi.libs.boostedyaml.boostedyaml.settings.general.GeneralSettings;
+import com.artillexstudios.axapi.libs.boostedyaml.boostedyaml.settings.loader.LoaderSettings;
+import com.artillexstudios.axapi.libs.boostedyaml.boostedyaml.settings.updater.UpdaterSettings;
+import com.artillexstudios.axapi.utils.MessageUtils;
+import org.bstats.bukkit.Metrics;
+
+import java.io.File;
+
+public final class GensFarm extends AxPlugin {
+
+    private static AxPlugin instance;
+    public static Config CONFIG;
+    public static Config MESSAGES;
+    public static MessageUtils MESSAGEUTILS;
+
+    public static AxPlugin getInstance() {
+        return instance;
+    }
+
+    public void enable() {
+        instance = this;
+
+        int pluginID = 50100;
+        new Metrics(this, pluginID);
+
+        CONFIG = new Config(new File(getDataFolder(), "config.yml"), getResource("config.yml"), GeneralSettings.builder().setUseDefaults(false).build(), LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("version")).build());
+        MESSAGES = new Config(new File(getDataFolder(), "messages.yml"), getResource("messages.yml"), GeneralSettings.builder().setUseDefaults(false).build(), LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("version")).build());
+        MESSAGEUTILS = new MessageUtils(MESSAGES.getBackingDocument(), "prefix", CONFIG.getBackingDocument());
+
+    }
+
+    public void disable() {
+    }
+}
